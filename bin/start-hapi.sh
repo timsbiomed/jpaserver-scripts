4402c7d4ba5fc5ce7a07c175a495432ed6ff319f
Shahim Essaid <shahim@HAPI-N3C-Ubuntu.esgm31yurvhunmow51vaclwugh.cx.internal.cloudapp.net>
2022-05-12 17:23:30 +0000

    Initial working setup, with many .gitignores

---
+++ bin/start-hapi.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -x
+set -e
+set -u
+set -o pipefail
+set -o noclobber
+#set -f # no globbing
+#shopt -s failglob # fail if glob doesn't expand
+
+# See http://stackoverflow.com/questions/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+source "${DIR}/source.sh"
+
+
+exec java -jar "${DIR}/../hapi/hapi.jar" --spring.config.location="${DIR}/../hapi/"
