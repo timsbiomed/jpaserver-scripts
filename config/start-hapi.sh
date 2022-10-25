130b86d2b5a1a20b768d9c4fc782aecbf28ddaf6
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-10-25 08:46:45 -0600

    WIP added path to ROOT.war

--- config/start-hapi.sh
+++ config/start-hapi.sh
@@ -21,5 +21,6 @@ if [ -f "$DIR/../bin/.env-local" ]; then
 	source "$DIR/../bin/.env-local"
 fi
 
+#wget https://github.com/HOT-Ecosystem/hapi-fhir-jpaserver-starter/releases/download/Oct_24_2022/ROOT.war
 
 exec java -Xmx10G -jar "${DIR}/../hapi/hapi.jar" --spring.config.location="${DIR}/../hapi/"
