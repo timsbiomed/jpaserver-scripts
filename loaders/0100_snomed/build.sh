69a3a6dd957b065fca700395ffea8b3d9c223370
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-10-25 10:05:04 -0600

    Changed build.sh scripts for cached ontologies. You just get a message saying it's not there if that's the case

--- loaders/0100_snomed/build.sh
+++ loaders/0100_snomed/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+# See http://stackoverflow.com/questions/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
 $FILE="SnomedCT_USEditionRF2_PRODUCTION_20210901T120000Z.zip"
-if [[ ! -e $FILE] ; then
-    echo "ERROR no file. SNOMED download TBD, need to deal with logins."
+if [[ ! -e ../../ontology_cache/$FILE] ; then
+    echo "ERROR no file in ontology_cache. $FILE"
     exit 1;
 fi
