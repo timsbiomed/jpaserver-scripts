e57c5e08018142adb1ae506a85963005cea236e3
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-10-25 10:05:04 -0600

    Changed build.sh scripts for cached ontologies. You just get a message saying it's not there if that's the case

--- loaders/0110_loinc/build.sh
+++ loaders/0110_loinc/build.sh
@@ -2,8 +2,18 @@
 
 # dowload LOINC 2.72 (version is important, 2.73 breaks the HAPI LOINC loader as of 10/2022)
 
+# See http://stackoverflow.com/questions/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
 FILE="Loinc_2.72.zip"
-if [[ ! -f $FILE ]] ; then
-    echo "ERROR no file. LOINC download TBD, need to deal with logins."
+if [[ ! -f ../../ontology_cache/$FILE ]] ; then
+    echo "ERROR no file in ontology_cache. $FILE"
     exit 1;
 fi
