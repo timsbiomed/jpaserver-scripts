edc32c066010ac64b2ef6a66b6238e08c9fc131f
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-10-27 16:28:56 -0600

    switched r4 to fhir

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,1 +1,11 @@
-export HAPI_R4="http://localhost:8000/r4"
+export HAPI_R4="http://localhost:8080/fhir"
+
+
+# mac
+TIMSTS_DIR=/Users/roederc/work/git_tims/jpaserver-scripts
+
+export PGHOST=localhost
+export PGPORT=5432
+export PGDATABASE=hapifhir
+export PGUSER=postgres
+export PGPASSWORD=it_matters_not
