89f492dd02855ff03da100ec7bfde232663bfb5f
Chris Roeder <chris.roeder@cuanschutz.edu>
2022-11-09 13:00:21 -0700

    added PG_ environment variables, HAPI_R4 url base too

--- bin/profile.sh
+++ bin/profile.sh
@@ -1,5 +1,6 @@
 #export HAPI_R4="http://localhost:8080/fhir"
-export HAPI_R4="http://localhost:8088/fhir"
+#export HAPI_R4="http://localhost:8088/fhir"
+export HAPI_R4="http://localhost:8000/fhir"
 
 
 # mac
@@ -10,3 +11,10 @@ export PGPORT=5432
 export PGDATABASE=hapifhir
 export PGUSER=postgres
 export PGPASSWORD=it_matters_not
+
+export PG_HOST=$PGHOST
+export PG_PORT=$PGPORT
+export PG_DATABASE=$PGDATABASE
+export PG_DB=$PGDATABASE
+export PG_USER=$PGUSER
+export PG_PASSWORD=$PGPASSWORD
